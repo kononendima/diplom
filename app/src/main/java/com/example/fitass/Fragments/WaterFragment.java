b92ac425ce492d685bce7a836a88c0a4999ce84e
KononenkoDmiriy <48153076+KononenkoDmiriy@users.noreply.github.com>
2020-05-18 03:37:21 +0300

    rdy

--- app/src/main/java/com/example/fitass/Fragments/WaterFragment.java
+++ app/src/main/java/com/example/fitass/Fragments/WaterFragment.java
@@ -9,12 +9,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.fitass.R;
+import com.example.fitass.eatlist.EatItemManager;
+import com.example.fitass.eatlist.EatListAdapter;
+
+import butterknife.ButterKnife;
+
 public class WaterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return null;
+        eatItemManager = new EatItemManager(getActivity());
+        eatItems = eatItemManager.getEatItemsList();
+        View v=inflater.inflate(R.layout.activity_eat_list, null);
+        ButterKnife.bind(this,v);
+
+        btnAdd.setOnClickListener(this);
+
+        // создаем адаптер
+        adapterEat = new EatListAdapter(getActivity(), eatItems);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapterEat);
     }
 }
